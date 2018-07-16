ffbae6650f5853c373001fd9ff5e629c3e99063d
EricaTheDesigner <Erica@EricaTheDesigner.com>
2018-07-15 21:46:54 -0400

    list_item.xml added

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -67,12 +69,12 @@ public class NumbersActivity extends AppCompatActivity {
 //        }
 
         //For statement
-        for (int i = 0; i < words.size(); i++){
-            LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(i));
-            rootView.addView(wordView);
-        }
+//        for (int i = 0; i < words.size(); i++){
+//            LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(i));
+//            rootView.addView(wordView);
+//        }
 
 //        //print message to test values
 //        Log.v("NumbersActivity", "Word at index 3: " + words.get(3));
@@ -80,6 +82,12 @@ public class NumbersActivity extends AppCompatActivity {
 //        TextView wordView2 = new TextView(this);
 //        wordView2.setText(words.get(1));
 //        rootView.addView(wordView2);
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
 
     }
 }
