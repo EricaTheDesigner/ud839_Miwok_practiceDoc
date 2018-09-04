3cd49b194073334d0452e5033b8105d292934e4f
EricaTheDesigner <Erica@EricaTheDesigner.com>
2018-09-03 23:06:36 -0400

    Edit WordAdapter.java [currentWord]

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,17 +23,32 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Check if the existing view is being reused, otherwise inflate the view
         View listItemView = convertView;
-
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
+
+        //Get the {@Link Word} object located at this position in the list
         Word my_word = (Word) getItem(position);
+
+        //Get the Textview in the list_item.xml layout with the ID miwok_text_view.
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+
+        //Get the Miwok translation from the currentWord object and set this text on the Miwok TextView.
         miwokTextView.setText(my_word.getMiwokTranslation());
+
+        //Find the TextView in the list_item.xml layout with the ID default_text_view.
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+
+        //Get the default translation from the currentWord object and set this text on the default TextView.
         defaultTextView.setText(my_word.getmDefaultTranslation());
 
+        //Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        //Set the ImageView to the image resource specified in the current Word
+        imageView.setImageResource(currentWord.getImageResourceId());
+
+        //Return the whole list item layout (containing 2 TextViews) so that it can be shown in the ListView
         return listItemView;
     }
 }
